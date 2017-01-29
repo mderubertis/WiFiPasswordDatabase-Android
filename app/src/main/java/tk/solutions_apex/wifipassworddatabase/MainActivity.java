9ab6ecf9220f0bf3d63302e0e7cbd5498e5c9dd0
Michael De Rubertis <mderubertis@mikesystems.tk>
2017-01-29 17:49:43 -0500

    Changed New Network Fragment to Activity. Added form for New Network

--- app/src/main/java/tk/solutions_apex/wifipassworddatabase/MainActivity.java
+++ app/src/main/java/tk/solutions_apex/wifipassworddatabase/MainActivity.java
@@ -65,8 +65,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, NewNetworkActivity.class));
             }
         });
 
