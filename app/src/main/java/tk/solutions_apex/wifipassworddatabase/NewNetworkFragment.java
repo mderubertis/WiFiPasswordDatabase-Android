f29864cee30a42050343a68e9498edda35bab9b9
Michael De Rubertis <mderubertis@mikesystems.tk>
2017-01-29 17:28:05 -0500

    Added Security section on about Fragement and added New Network Fragment

--- app/src/main/java/tk/solutions_apex/wifipassworddatabase/NewNetworkFragment.java
+++ app/src/main/java/tk/solutions_apex/wifipassworddatabase/NewNetworkFragment.java
@@ -1,30 +1,35 @@
 package tk.solutions_apex.wifipassworddatabase;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AboutFragment.OnFragmentInteractionListener} interface
+ * {@link NewNetworkFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AboutFragment#newInstance} factory method to
+ * Use the {@link NewNetworkFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AboutFragment extends Fragment {
+public class NewNetworkFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    public AboutFragment() {
+    public NewNetworkFragment() {
         // Required empty public constructor
     }
 
@@ -32,35 +37,52 @@ public class AboutFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @return A new instance of fragment AboutFragment.
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NewNetworkFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
+    public static NewNetworkFragment newInstance(String param1, String param2) {
+        NewNetworkFragment fragment = new NewNetworkFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_about,
-                container, false);
+        return inflater.inflate(R.layout.fragment_new_network, container, false);
+    }
 
-        ((ImageView) view.findViewById(R.id.imgAcctCreate)).setImageBitmap(BitmapFactory.decodeResource(getResources(), R.raw.acct_create));
-        ((ImageView) view.findViewById(R.id.imgTxRx)).setImageBitmap(BitmapFactory.decodeResource(getResources(), R.raw.data_txrx));
-        return view;
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
     }
 
     @Override
